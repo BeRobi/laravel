52c5f07d4a3221bae4b978fb104cb1aad1d7198c
BeRobi <screengsm@gmail.com>
2023-10-11 20:01:48 +0200

    adatbázis kész

--- kepzes/database/migrations/2023_10_11_175024_create_tasks_table.php
+++ kepzes/database/migrations/2023_10_11_175024_create_tasks_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-
-            //elsődleges kulcs, id a neve, auto-increment, bigint típusú
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title', 50);
+            $table->longtext('description');
+            $table->date('end date');
+            $table->boolean('status');
+            //külső kulcs, létre is hozza a mezőt
+            $table->foreignID('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
